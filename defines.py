c19748b32e0cd462f12d6e8f03b782e8d553203c
Fávero Santos <favero.santos@velsis.com.br>
2021-11-29 14:29:21 -0300

    1. Adição de fila de processamento 2. Decodificação de novas mensagens 3. Ajuste de código para utilização das variáveis locais (dos processos).

--- defines.py
+++ defines.py
@@ -3,7 +3,13 @@
 DEBUG_MODE = True
 
 # Dados importantes para o protocolo do Doppler
+'Fonte: 2019-02-22_Doc_Data Communication UMRR Traffic Management_UMRR0C'
 HEADER = bytearray([0xCA, 0xCB, 0xCC, 0xCD])
-FOOTER = bytearray([0xFA, 0xFB, 0xFC, 0xFD])
+FOOTER = bytearray([0xEA, 0xEB, 0xEC, 0xED])
+OBJECT_CONTROL = bytearray([0x05, 0x01])
+OBJECT_DATA_1 = bytearray([0x05, 0x02])
+SYNC_MESSAGE = bytearray([0x02, 0xFF])
+SENSOR_CONTROL = bytearray([0x05, 0x00])
+
 REFRESH_TIME_MS = 0.5
 
--- processor.py
+++ processor.py
@@ -1,6 +1,8 @@
 from udpserver import udpServer
 import defines as defines
 import multiprocessing
+from multiprocessing import Queue
+
 import time
 
 if defines.DEBUG_MODE is True:
@@ -17,10 +19,14 @@ def udpWorker(name: str) -> None:
 class Processor:
 
     myUDPServer = 0
-    package = bytearray()
-    newMessage = False
+    #package = bytearray()
+
     processesList = list()
+
     packageReady = False
+
+    myQueue = Queue()
+    'myQueue é a fila de pacotes divida entre os dois processos'
 
     def __init__(self):
         print("Processor says: I am alive!")
@@ -29,41 +35,80 @@ class Processor:
         self.myUDPServer.connect()
 
         if defines.DEBUG_MODE is True:
-
             udpClientProcess = multiprocessing.Process(target=udpWorker, args=("udpClient",))
             self.processesList.append(udpClientProcess)
             udpClientProcess.start()
 
-        udpServerListener = multiprocessing.Process(target=self.listen, args=("udpServerListener",))
+        udpServerListener = multiprocessing.Process(target=self.listen, args=("udpServerListener", self.myQueue))
         self.processesList.append(udpServerListener)
         udpServerListener.start()
 
-        udpServerProcessor = multiprocessing.Process(target=self.process, args=("udpServerProcessor",))
+        udpServerProcessor = multiprocessing.Process(target=self.process, args=("udpServerProcessor", self.myQueue))
         self.processesList.append(udpServerProcessor)
         udpServerProcessor.start()
 
-    def listen(self, name: str) -> None:
+    def listen(self, name, myQueue):
         'This is the multi-process version of the listen function. It listens and create a package from the messages received by UDP'
+
+        newMessage = False
+        package = bytearray()
+
         while True:
             receivedData = self.myUDPServer.getData()
-            if receivedData[0:4] == defines.HEADER and self.newMessage is False:
-                self.newMessage = True
+            if receivedData[0:4] == defines.HEADER and newMessage is False:
+                newMessage = True
+                package = receivedData[4:len(receivedData)]
 
-            elif receivedData[len(receivedData)-4:len(receivedData)] == defines.FOOTER and self.newMessage is True:
-                self.newMessage = False
-                self.package.clear()
+            elif receivedData[len(receivedData)-4:len(receivedData)] == defines.FOOTER and newMessage is True:
+                newMessage = False
+
+                myQueue.put(package)
+
+                package = bytearray()
+
+            elif newMessage is True:
+                # TODO: Deve existir uma verificação de primeira mensagem aqui para que não fique acumulando mensagens indevidas
+                package = package + receivedData
             else:
-                self.package = self.package + receivedData
-                print("Mesangem recebida é:" + str(self.package))
-                self.packageReady = True
+                pass
             time.sleep(0.1)
 
-    def process(self, name: str) -> None:
+    def process(self, name, myQueue):
         print("Processor 'process' function is working!")
-        'This function is responsible for processing the data made available at listen function'
+        'This function is responsible for processing data made available from the listen function'
         while True:
-            self.packageReady = False #TODO: implementar como mutex a flag packageRDY
+            messageToProcess = myQueue.get()
+
+            # TODO: aqui seria legal implementar usando uma pesquisa em array
+
+
+            print("Mensagem para ser processada (ANTES): " + str(messageToProcess))
+
+            retries = 3
+            while retries > 0:
+                if messageToProcess[0:2] == defines.OBJECT_CONTROL:
+                    print("OBJECT_CONTROL recebido!")
+                    messageToProcess = messageToProcess[11:]
+                    # Faz o que tem que fazer, remove da lista
+                elif messageToProcess[0:2] == defines.SENSOR_CONTROL:
+                    print("SENSOR_CONTROL recebido!")
+                    messageToProcess = messageToProcess[11:]
+                    # Faz o que tem que fazer, remove da lista
+                elif messageToProcess[0:2] == defines.OBJECT_DATA_1:
+                    print("OBJECT_CONTROL recebido!")
+                    messageToProcess = messageToProcess[11:]
+                    # Faz o que tem que fazer, remove da lista
+                elif messageToProcess[0:2] == defines.SYNC_MESSAGE:
+                    print("SYNC_MESSAGE recebido!")
+                    messageToProcess = messageToProcess[11:]
+                    # Faz o que tem que fazer, remove da lista
+                print("Mensagem para ser processada (DEPOIS): " + str(messageToProcess))
+                retries = retries - 1
+
+            #self.packageList.pop(0)
+
             time.sleep(0.1)
+
 
     #def listen(self):
     #    'This is the original version of the listen function. It listens and create a package from the messages received by UDP'
